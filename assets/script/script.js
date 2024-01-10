2333f96eb91322863facff63a6a4eb9817f7bc15
Melody Miao <melodymiao@berkeley.edu>
2024-01-10 01:37:39 -0800

    portfolio additions

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,3 +5,31 @@ function scrollToSection(sectionId) {
   }
 }
   
+document.addEventListener("DOMContentLoaded", function () {
+  const circles = document.querySelectorAll('.sidebar-circle');
+  const sections = document.querySelectorAll('.sections');
+
+  function setActiveCircle(index) {
+    circles.forEach((circle, i) => {
+      if (i === index) {
+        circle.classList.add('active');
+      } else {
+        circle.classList.remove('active');
+      }
+    });
+  }
+
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.clientHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        setActiveCircle(index);
+      }
+    });
+  }
+
+  window.addEventListener('scroll', handleScroll);
+});
